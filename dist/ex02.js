259b442abc16947c7bd6530ccfa3c86a003ab442
Sae Nakatani <nakatanisae0218@gmail.com>
2026-08-14 00:49:24 -0700

    Complete ex02

--- dist/ex02.js
+++ dist/ex02.js
@@ -1,11 +1,8 @@
+"use strict";
 // Exercise 2: Create and Manipulate an Array
-
 // 1. Create an array called `fruits` containing strings representing different fruits.
 // 2. Add a new fruit to the array.
 // 3. Print out the list of fruits.
-
-const fruits:string[] = ["orange", "apple"];
-fruits.push("banana")
-
-
+const fruits = ["orange", "apple"];
+fruits.push("banana");
 console.log(fruits);
