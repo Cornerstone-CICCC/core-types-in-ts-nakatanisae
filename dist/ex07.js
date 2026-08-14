6c10c0439f66ebce91f53dc5bf1e31a7034945e2
Sae Nakatani <nakatanisae0218@gmail.com>
2026-08-14 09:36:45 -0700

    Complete ex07

---
+++ dist/ex07.js
@@ -0,0 +1,13 @@
+"use strict";
+// Exercise 7: Using Literal Types in Function
+// 1. Create a function called `printDirection` that accepts a direction parameter with a literal type of "left" or "right."
+// 2. Inside the function, print a message based on the provided direction.
+function printDirection(direction) {
+    if (direction === "left") {
+        console.log(`Direction : ${direction}`);
+    }
+    else if (direction === "right") {
+        console.log(`Direction : ${direction}`);
+    }
+}
+printDirection("right");
--- exercises/ex07.ts
+++ exercises/ex07.ts
@@ -2,3 +2,16 @@
 
 // 1. Create a function called `printDirection` that accepts a direction parameter with a literal type of "left" or "right."
 // 2. Inside the function, print a message based on the provided direction.
+
+function printDirection (
+    direction: "left" | "right"
+):void{
+    if(direction === "left"){
+        console.log(`Direction : ${direction}`)
+    } else if(direction === "right") {
+        console.log(`Direction : ${direction}`)
+    }
+    
+}
+
+printDirection("right")
