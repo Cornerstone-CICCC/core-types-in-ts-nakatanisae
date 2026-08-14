75d5839337aa8dd00d7d049c4e23217dfb3ed338
Sae Nakatani <nakatanisae0218@gmail.com>
2026-08-14 08:50:18 -0700

    Complete ex04

--- dist/ex04.js
+++ dist/ex04.js
@@ -1,23 +1,13 @@
+"use strict";
 // Exercise 4: Create a Tuple
-
 // 1. Create a tuple called `coordinate` with two elements: a number representing the x-coordinate and a number representing the y-coordinate.
 // 2. Assign values to the tuple elements.
 // 3. Create an interface of location with the properties `description` and the tuple that you created.
-
-
 // 4. Print out the interface and coordinates.
-
-const coordinate :[number, number] = [10, 20]
-    
-interface location {
-    description: string,
-    coordinate: [number, number]
-}
-
-const myLocation: location = {
+const coordinate = [10, 20];
+const myLocation = {
     description: "Vancouver",
     coordinate: coordinate
 };
-
 console.log("location object: ", myLocation);
 console.log("Coordinate: ", myLocation.coordinate);
