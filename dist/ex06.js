d16eee3f75f81e2b9bd9c149345184e534fe58e4
Sae Nakatani <nakatanisae0218@gmail.com>
2026-08-14 09:21:04 -0700

    Complete ex06

--- dist/ex06.js
+++ dist/ex06.js
@@ -1,16 +1,15 @@
+"use strict";
 // Exercise 6: Union Types for Function Parameters
-
 // 1. Create a function called `printId` that accepts either a number or a string as a parameter.
 // 2. Inside the function, check the type of the parameter and print a message accordingly.
 // 3. Return the function accordingly with the type, `number` or `string`.
-
-function printId(parameter: number | string){
-    if(typeof parameter === "string"){
-        return `${parameter} is string`
-    } else {
-        return `${parameter} is number`
+function printId(parameter) {
+    if (typeof parameter === "string") {
+        return `${parameter} is string`;
+    }
+    else {
+        return `${parameter} is number`;
     }
 }
-
 console.log(printId(123));
 console.log(printId("text"));
